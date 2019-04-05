93022fe239d229e34918eb7442438d5484c0481a
Rafael Najjar <rafaelnajjar@gmail.com>
2019-04-04 21:41:10 -0300

    Refactored Database.py

--- Versão 1.0/Script Python/Database.py
+++ Versão 1.0/Script Python/Database.py
@@ -17,199 +17,113 @@ class Database:
                         database.close()
                 except pymysql.DatabaseError:
                         pass
-                        
-        def select_db(self, table):
+
+        def executeQuery(self, query, errorMsg)
                 db = self.connect()
                 if db != False:
                         try:
                                 curs = db.cursor()
-                                curs.execute("select * from " + table)
+                                curs.execute(query)
                                 for reading in curs.fetchall():
                                         for i in reading:
                                                 print (str(i) + "  ",end = "")
                                         print()
                         except pymysql.DatabaseError:
-                                print("It wasn't possible to gather the data from table")
+                                print(errorMsg)
                         self.disconnect(db)
+
+        def executeQueries(self, queries, errorMsg)
+                db = self.connect()
+                if db != False:
+                        try:
+                                curs = db.cursor()
+                                for query in queries
+                                        curs.execute(query)
+                                        for reading in curs.fetchall():
+                                                for i in reading:
+                                                        print (str(i) + "  ",end = "")
+                                                print()
+                        except pymysql.DatabaseError:
+                                print(errorMsg)
+                        self.disconnect(db)
+
+               
+        def select_db(self, table):
+                query = "select * from " + table
+                errorMsg = "It wasn't possible to gather the data from table"
+                executeQuery(self, query, errorMsg)
                                 
         def insert_db(self,table,matriculation,ncard,name):
-                db = self.connect()
-                if db != False:
-                        try:
-                                curs = db.cursor()
-                                curs.execute("insert into " + table + " values(0,'" + str(matriculation) + "'," + str(ncard) + ",'" + str(name) + "', now())")
-                                db.commit()
-                        except pymysql.DatabaseError:
-                                db.rollback()
-                                print("It wasn't possible to insert the " + table[2:] + " in the table")
-                        self.disconnect(db)
-
+                query = "insert into " + table + " values(0,'" + str(matriculation) + "'," + str(ncard) + ",'" + str(name) + "', now())"
+                errorMsg = "It wasn't possible to insert the " + table[2:] + " in the table"
+                executeQuery(self, query, errorMsg)
+                
         def delete_db_name(self,table,arg):
-                db = self.connect()
-                if db != False:
-                        try:
-                                curs = db.cursor()
-                                curs.execute("delete from " + table + " where TXT_NAME = '" + str(arg) + "'")
-                                db.commit()
-                        except pymysql.DatabaseError:
-                                db.rollback()
-                                print("It wasn't possible to delete the " + table[2:] + " from the table")
-                        self.disconnect(db)
-
+                query = "delete from " + table + " where TXT_NAME = '" + str(arg) + "'"
+                errorMsg = "It wasn't possible to delete the " + table[2:] + " from the table"
+                executeQuery(self, query, errorMsg)
+                
         def validate_teacher(self,classroom,value):
                 str_select = "select l.ID, l.DAT_DAY_OF_LECTURE, sc.ID_CLASS, te.NUM_CARD, te.NUM_MATRICULATION,s.DAT_END "
                 str_from = "from T_LECTURE l, T_SCHEDULE_CLASS sc, T_SCHEDULE s, T_TEACHER_CLASS tc, T_TEACHER te, T_CLASSROOM c, T_CLASSROOM_CLASS cc "
                 str_where = "where TIME(NOW()) < TIME(s.DAT_END) AND DATE(NOW()) = DATE(l.DAT_DAY_OF_LECTURE) AND l.ID_SCHEDULE_CLASS = sc.ID AND cc.ID_CLASSROOM = c.ID AND cc.ID_CLASS = sc.ID_CLASS AND sc.ID_SCHEDULE = s.ID AND tc.ID_CLASS = sc.ID_CLASS AND tc.ID_TEACHER = te.ID AND te.DAT_REMOVED is null AND (te.NUM_MATRICULATION = '" + value + "' OR te.NUM_CARD = '" + value + "') AND c.TXT_ROOM = '" + classroom + "' "
                 str_order_limit = "order by s.DAT_END limit 1;"
-                db = self.connect()
-                if db != False:
-                        try:
-                                curs = db.cursor()
-                                curs.execute(str_select + str_from + str_where + str_order_limit)
-                                result_row = curs.fetchone()
-                                print(result_row)
-                                if result_row:
-                                        return True
-                                else:
-                                        return False
-                        except pymysql.DatabaseError:
-                                db.rollback()
-                                print("It wasn't possible to validate teacher")
-                                return False
-                        self.disconnect(db)
-
+                query = str_select + str_from + str_where + str_order_limit
+                errorMsg = "It wasn't possible to validate teacher"
+                executeQuery(self, query, errorMsg)
                 
-
         def delete_db_matriculation(self,table,arg):
-                db = self.connect()
-                if db != False:
-                        try:
-                                curs = db.cursor()
-                                curs.execute("delete from " + table + " where NUM_MATRICULATION = '" + str(arg) + "'")
-                                db.commit()
-                        except pymysql.DatabaseError:
-                                db.rollback()
-                                print("It wasn't possible to delete the " + table[2:] + " from the table")
-                        self.disconnect(db)
-
+                query = "delete from " + table + " where NUM_MATRICULATION = '" + str(arg) + "'"
+                errorMsg = "It wasn't possible to delete the " + table[2:] + " from the table"
+                executeQuery(self, query, errorMsg)
+                
         def delete_db_card(self,table,arg):
-                db = self.connect()
-                if db != False:
-                        try:
-                                curs = db.cursor()
-                                curs.execute("delete from " + table + " where NUM_CARD = " + str(arg))
-                                db.commit()
-                        except pymysql.DatabaseError:
-                                db.rollback()
-                                print("It wasn't possible to delete the " + table[2:] + " from the table")
-                        self.disconnect(db)
-
+                query = "delete from " + table + " where NUM_CARD = " + str(arg)
+                errorMsg = "It wasn't possible to delete the " + table[2:] + " from the table"
+                executeQuery(self, query, errorMsg)
+                
         def update_db_name_name(self,table,new,old):
-                db = self.connect()
-                if db != False:
-                        try:
-                                curs = db.cursor()
-                                curs.execute("update " + table + " set TXT_NAME = '" + str(new) + "' where TXT_NAME = '" + str(old) + "'")
-                                db.commit()
-                        except pymysql.DatabaseError:
-                                db.rollback()
-                                print("It wasn't possible to update the " + table[2:] + " in the table")
-                        self.disconnect(db)
-
+                query = "update " + table + " set TXT_NAME = '" + str(new) + "' where TXT_NAME = '" + str(old) + "'"
+                errorMsg = "It wasn't possible to update the " + table[2:] + " in the table"
+                executeQuery(self, query, errorMsg)
+                
         def update_db_name_matriculation(self,table,new,old):
-                db = self.connect()
-                if db != False:
-                        try:
-                                curs = db.cursor()
-                                curs.execute("update " + table + " set TXT_NAME = '" + str(new) + "' where NUM_MATRICULATION = '" + str(old) + "'")
-                                db.commit()
-                        except pymysql.DatabaseError:
-                                db.rollback()
-                                print("It wasn't possible to update the " + table[2:] + " in the table")
-                        self.disconnect(db)
-
-
+                query = "update " + table + " set TXT_NAME = '" + str(new) + "' where NUM_MATRICULATION = '" + str(old) + "'"
+                errorMsg = "It wasn't possible to update the " + table[2:] + " in the table"
+                executeQuery(self, query, errorMsg)
+                
         def update_db_name_card(self,table,new,old):
-                db = self.connect()
-                if db != False:
-                        try:
-                                curs = db.cursor()
-                                curs.execute("update " + table + " set TXT_NAME = '" + str(new) + "' where NUM_CARD = " + str(old))
-                                db.commit()
-                        except pymysql.DatabaseError:
-                                db.rollback()
-                                print("It wasn't possible to update the " + table[2:] + " in the table")
-                        self.disconnect(db)
+                query = "update " + table + " set TXT_NAME = '" + str(new) + "' where NUM_CARD = " + str(old)
+                errorMsg = "It wasn't possible to update the " + table[2:] + " in the table"
+                executeQuery(self, query, errorMsg)
                         
         def update_db_matriculation_matriculation(self,table,new,old):
-                db = self.connect()
-                if db != False:
-                        try:
-                                curs = db.cursor()
-                                curs.execute("update " + table + " set NUM_MATRICULATION = '" + str(new) + "' where NUM_MATRICULATION = '" + str(old) + "'")
-                                db.commit()
-                        except pymysql.DatabaseError:
-                                db.rollback()
-                                print("It wasn't possible to update the " + table[2:] + " in the table")
-                        self.disconnect(db)
-
+                query = "update " + table + " set NUM_MATRICULATION = '" + str(new) + "' where NUM_MATRICULATION = '" + str(old) + "'"
+                errorMsg = "It wasn't possible to update the " + table[2:] + " in the table"
+                executeQuery(self, query, errorMsg)
+                
         def update_db_matriculation_name(self,table,new,old):
-                db = self.connect()
-                if db != False:
-                        try:
-                                curs = db.cursor()
-                                curs.execute("update " + table + " set NUM_MATRICULATION = '" + str(new) + "' where TXT_NAME = '" + str(old) + "'")
-                                db.commit()
-                        except pymysql.DatabaseError:
-                                db.rollback()
-                                print("It wasn't possible to update the " + table[2:] + " in the table")
-                        self.disconnect(db)
+                query = "update " + table + " set NUM_MATRICULATION = '" + str(new) + "' where TXT_NAME = '" + str(old) + "'"
+                errorMsg = "It wasn't possible to update the " + table[2:] + " in the table"
+                executeQuery(self, query, errorMsg)
                         
         def update_db_matriculation_card(self,table,new,old):
-                db = self.connect()
-                if db != False:
-                        try:
-                                curs = db.cursor()
-                                curs.execute("update " + table + " set NUM_MATRICULATION = '" + str(new) + "' where NUM_CARD = " + str(old))
-                                db.commit()
-                        except pymysql.DatabaseError:
-                                db.rollback()
-                                print("It wasn't possible to update the " + table[2:] + " in the table")
-                        self.disconnect(db)
-
+                query = "update " + table + " set NUM_MATRICULATION = '" + str(new) + "' where NUM_CARD = " + str(old)
+                errorMsg = "It wasn't possible to update the " + table[2:] + " in the table"
+                executeQuery(self, query, errorMsg)
+                
         def update_db_card_card(self,table,new,old):
-                db = self.connect()
-                if db != False:
-                        try:
-                                curs = db.cursor()
-                                curs.execute("update " + table + " set NUM_CARD = " + str(arg1) + " where NUM_CARD = " + str(arg2))
-                                db.commit()
-                        except pymysql.DatabaseError:
-                                db.rollback()
-                                print("It wasn't possible to update the " + table[2:] + " in the table")
-                        self.disconnect(db)
-        
+                query = "update " + table + " set NUM_CARD = " + str(arg1) + " where NUM_CARD = " + str(arg2)
+                errorMsg = "It wasn't possible to update the " + table[2:] + " in the table"
+                executeQuery(self, query, errorMsg)
+                
         def update_db_card_name(self,table,new,old):
-                db = self.connect()
-                if db != False:
-                        try:
-                                curs = db.cursor()
-                                curs.execute("update " + table + " set NUM_CARD = " + str(arg1) + " where TXT_NAME = '" + str(old) + "'")
-                                db.commit()
-                        except pymysql.DatabaseError:
-                                db.rollback()
-                                print("It wasn't possible to update the " + table[2:] + " in the table")
-                        self.disconnect(db)     
-        
-        def update_db_card_matriculation(self,table,new,old):
-                db = self.connect()
-                if db != False:
-                        try:
-                                curs = db.cursor()
-                                curs.execute("update " + table + " set NUM_CARD = " + str(arg1) + " where NUM_MATRICULATION = '" + str(old) + "'")
-                                db.commit()
-                        except pymysql.DatabaseError:
-                                db.rollback()
-                                print("It wasn't possible to update the " + table[2:] + " in the table")
-                        self.disconnect(db)
+                query = "update " + table + " set NUM_CARD = " + str(arg1) + " where TXT_NAME = '" + str(old) + "'"
+                errorMsg = "It wasn't possible to update the " + table[2:] + " in the table"
+                executeQuery(self, query, errorMsg)
 
+        def update_db_card_matriculation(self,table,new,old):
+                query = "update " + table + " set NUM_CARD = " + str(arg1) + " where NUM_MATRICULATION = '" + str(old) + "'"
+                errorMsg = "It wasn't possible to update the " + table[2:] + " in the table"
+                executeQuery(self, query, errorMsg)
+                
